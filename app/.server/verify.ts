5aa430e1535ff4b8fb95cfbf03a9568b0dd26958
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2025-02-14 12:15:09 -0500

    feat: add verification for otp

---
+++ app/.server/verify.ts
@@ -0,0 +1,98 @@
+import { parseWithZod } from '@conform-to/zod';
+import { verifyTOTP } from '@epic-web/totp';
+import { json } from '@remix-run/node';
+import { z } from 'zod';
+import { prisma } from './db';
+
+interface Verification {
+  code: string;
+  type: 'sign-up';
+  target: string;
+}
+
+export const TYPE_QUERY_PARAM = 'type';
+export const TARGET_QUERY_PARAM = 'target';
+export const CODE_QUERY_PARAM = 'code';
+export const REDIRECT_TO_QUERY_PARAM = 'redirectTo';
+
+const VerifySchema = z.object({
+  [CODE_QUERY_PARAM]: z.string().min(5).max(5),
+  [TYPE_QUERY_PARAM]: z.enum(['sign-up']), // add more types of verification here
+  [TARGET_QUERY_PARAM]: z.string(),
+  [REDIRECT_TO_QUERY_PARAM]: z.string().optional(),
+});
+
+export async function validateRequest(request: Request, body: URLSearchParams) {
+  const submission = await parseWithZod(body, {
+    async: true,
+    schema: VerifySchema.transform(async ({ code, target, type }, ctx) => {
+      const codeIsValid = await isCodeValid({ code, type, target });
+
+      if (!codeIsValid) {
+        ctx.addIssue({
+          path: ['code'],
+          code: z.ZodIssueCode.custom,
+          message: 'Invalid code',
+        });
+
+        return;
+      }
+    }),
+  });
+
+  if (submission.status !== 'success') {
+    return json({ result: submission.reply() }, { status: submission.status === 'error' ? 400 : 200 });
+  }
+
+  const submissionValue = submission.value;
+
+  console.log('submissionValue', submissionValue);
+
+  async function deleteVerification() {
+    await prisma.verification.delete({
+      where: {
+        target_type: {
+          target: submissionValue[TARGET_QUERY_PARAM],
+          type: submissionValue[TYPE_QUERY_PARAM],
+        },
+      },
+    });
+  }
+
+  switch (submissionValue[TYPE_QUERY_PARAM]) {
+    case 'sign-up': {
+      await deleteVerification();
+      return {};
+    }
+  }
+}
+
+export async function isCodeValid({ code, type, target }: Verification) {
+  const verification = await prisma.verification.findUnique({
+    where: {
+      target_type: { target, type },
+    },
+    select: {
+      secret: true,
+      period: true,
+      digits: true,
+      algorithm: true,
+      charSet: true,
+    },
+  });
+
+  if (!verification) {
+    return false;
+  }
+
+  const result = await verifyTOTP({
+    otp: code,
+    ...verification,
+  });
+
+  if (!result) {
+    return false;
+  } else {
+    return true;
+  }
+}
--- app/routes/_auth+/verify.tsx
+++ app/routes/_auth+/verify.tsx
@@ -1,25 +1,20 @@
 import { LoaderFunctionArgs } from '@remix-run/node';
 import { requireAnonymous } from '~/.server/auth';
+import { CODE_QUERY_PARAM, validateRequest } from '~/.server/verify';
 import { H1 } from '~/components/typography';
-
-export const TYPE_QUERY_PARAM = 'type';
-export const TARGET_QUERY_PARAM = 'target';
-export const CODE_QUERY_PARAM = 'code';
 
 export async function loader({ request }: LoaderFunctionArgs) {
   await requireAnonymous(request);
 
   const url = new URL(request.url);
   const params = url.searchParams;
-  const type = params.get(TYPE_QUERY_PARAM);
-  const target = params.get(TARGET_QUERY_PARAM);
 
-  //   Two things can happen here. The user could either verify by clicking the magic link or by entering the otp
+  // The user could either verify by clicking the magic link or by entering the otp
   if (!params.has(CODE_QUERY_PARAM)) {
     return {};
   }
 
-  return {};
+  return validateRequest(request, params);
 }
 
 export default function VerifyRoute() {
