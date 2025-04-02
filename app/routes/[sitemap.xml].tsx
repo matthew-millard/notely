5388e2260aa69dd942044fdb8ba38caf2b16ad10
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2025-04-02 00:12:20 -0400

    config: add sitemap route

---
+++ app/routes/[sitemap.xml].tsx
@@ -0,0 +1,42 @@
+export async function loader() {
+  const staticPages = [
+    { loc: '/', lastmod: '2025-04-01', changefreq: 'monthly', priority: 1.0 },
+    {
+      loc: '/privacy-policy/',
+      lastmod: '2025-04-02',
+      changefreq: 'yearly',
+      priority: 0.5,
+    },
+  ];
+  const baseUrl = process.env.NODE_ENV === 'production' ? process.env.BASE_URL : 'http://localhost:3000';
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${staticPages
+        .map(
+          page => `
+        <url>
+          <loc>${baseUrl}${page.loc}</loc>
+          <lastmod>${page.lastmod}</lastmod>
+          <changefreq>${page.changefreq}</changefreq>
+          <priority>${page.priority}</priority>
+        </url>
+      `
+        )
+        .join('')}
+    </urlset>`;
+
+  try {
+    return new Response(sitemap, {
+      headers: {
+        'Content-Type': 'application/xml',
+        'Cache-Control': 'public, max-age=86400', // Cache for a day
+      },
+      status: 200,
+    });
+  } catch (error) {
+    console.error('Error generating sitemap:', error);
+
+    throw new Response('Internal server error', { status: 500 });
+  }
+}
